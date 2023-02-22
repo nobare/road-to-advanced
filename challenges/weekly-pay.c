f67d4df13d49e309bdb5fd40d5b1ff812655965b
nobare <vinifalconi@gmail.com>
2023-02-22 18:17:22 -0300

    adjustments to code

--- challenges/weekly-pay.c
+++ challenges/weekly-pay.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PAYRATE 12.00
+#define TAXRATE_300 .15
+#define TAXRATE_150 .20
+#define TAXRATE_REST .25
+#define OVERTIME 40
+
+
 int main(int argc, char const *argv[])
 {
-    double hours, gross, taxes, net_pay;
+    int hours;
+    double gross, taxes, net_pay, overtime;
 
     printf("Enter the number of hours: ");
     scanf("%d", &hours);
 
     gross = 12 * hours;
 
-    if (gross <= 300) 
-        taxes = gross * 0.15;
-    else if (gross <= 450)
-        taxes = (300 * 0.15) + ((gross - 300) * 0.20);
-    else 
-        taxes = (300 * 0.15 ) + ((150/100) * 20) + (((gross - 450)/100) * 25);
+    if (hours <= 40)
+        gross = hours * PAYRATE;
+    else
+        overtime = (hours - 40) * (PAYRATE * 1.5);
+        gross = 40 * PAYRATE + overtime;
 
-    printf("Gross: %.2f, Taxes: %.2f, Net Pay: %.2f", gross, taxes, gross-taxes);
+    if (gross <= 300) 
+        taxes = gross * TAXRATE_300;
+    else if (gross <= 450)
+        taxes = (300 * TAXRATE_300) + ((gross - 300) * TAXRATE_150);
+    else 
+        taxes = (300 * TAXRATE_300) + (150 * TAXRATE_150) + ((gross - 450) * TAXRATE_REST);
+
+    net_pay = gross - taxes;
+    printf("Gross: %.2f, Taxes: %.2f, Net Pay: %.2f", gross, taxes, net_pay);
 
     return 0;
 }
