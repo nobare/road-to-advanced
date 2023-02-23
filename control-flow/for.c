42f43df95ac1712fadef002f31aaf3f50e35c190
nobare <vinifalconi@gmail.com>
2023-02-23 08:28:33 -0300

    for

--- control-flow/for.c
+++ control-flow/for.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char const *argv[])
+int main(int argc, char const *argv[]) 
 {
+    int iterador, condition;
+
+    condition = iterador;
+    for (iterador = 5; iterador > 0; --iterador)
+        printf("%d ", iterador);
     
+    for (iterador = 0; iterador < 10; ++iterador)
+        printf("%d ", iterador);
     return 0;
+
 }
