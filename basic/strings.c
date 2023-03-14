c2fc10ba794a4bd3ee227a2700a062b380288080
nobare <vinifalconi@gmail.com>
2023-03-13 23:37:05 -0300

    13-03-23

---
+++ basic/strings.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    //length of a string
+    strlen();
+    //copying one character string to another
+    strcpy(); strncpy();
+    //concatenation of strings
+    strcat(); strncat();
+    //checking if strings are equal
+    strcmp(); strncmp();
+    return 0;
+}
